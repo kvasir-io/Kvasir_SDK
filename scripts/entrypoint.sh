721154efb3cf84f3418642a9be1afb50a2c0676c
dominic <mail@dominicp.de>
2025-09-25 15:45:20 +0200

    entrypoint with docker and podman

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-USER_ID=${LOCAL_USER_ID:-50000}
-GROUP_ID=${LOCAL_GROUP_ID:-50000}
+if [ "$(id -u)" = "0" ]; then
+    # Running as root, create user
+    USER_ID=${LOCAL_USER_ID:-50000}
+    GROUP_ID=${LOCAL_GROUP_ID:-50000}
 
-groupadd -r -g $GROUP_ID container_user
-useradd --shell /bin/fish -u $USER_ID -g $GROUP_ID -o -m container_user
+    groupadd -r -g $GROUP_ID container_user
+    useradd --shell /bin/fish -u $USER_ID -g $GROUP_ID -o -m container_user
 
-export HOME=/home/container_user
-
-exec su-exec container_user "$@"
+    export HOME=/home/container_user
+    exec su-exec container_user "$@"
+else
+    # Already running as correct user just exec
+    exec "$@"
+fi
