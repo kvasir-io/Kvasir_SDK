0c111f6688e34f496be479d6a539465beb85d1ae
dominic <mail@dominicp.de>
2025-11-17 11:59:27 +0100

    Added another RAM_FUNC attribute

--- src/kvasir/Util/attributes.hpp
+++ src/kvasir/Util/attributes.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #ifdef __clang__
-    #define KVASIR_RAM_FUNC_ATTRIBUTES gnu::section(".data"), gnu::noinline
-    #define KVASIR_RESETISR_ATTRIBUTES noreturn
+    #define KVASIR_RAM_FUNC_ATTRIBUTES        gnu::section(".data"), gnu::noinline
+    #define KVASIR_RAM_FUNC_INLINE_ATTRIBUTES gnu::section(".data"), gnu::always_inline
+    #define KVASIR_RESETISR_ATTRIBUTES        noreturn
 #else
-    #define KVASIR_RAM_FUNC_ATTRIBUTES gnu::section(".data#"), gnu::noinline, gnu::long_call
-    #define KVASIR_RESETISR_ATTRIBUTES noreturn, gnu::naked
+    #define KVASIR_RAM_FUNC_ATTRIBUTES        gnu::section(".data#"), gnu::noinline, gnu::long_call
+    #define KVASIR_RAM_FUNC_INLINE_ATTRIBUTES gnu::section(".data"), gnu::always_inline
+    #define KVASIR_RESETISR_ATTRIBUTES        noreturn, gnu::naked
 #endif
