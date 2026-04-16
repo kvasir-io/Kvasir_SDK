ed335a5d8dbb15d815db508a75c7f9076e58297f
dominic <mail@dominicp.de>
2026-04-16 15:24:38 +0200

    move register fmt

--- src/kvasir/Register/Register.hpp
+++ src/kvasir/Register/Register.hpp
@@ -151,34 +151,3 @@ namespace Kvasir { namespace Register {
     };
 
 }}   // namespace Kvasir::Register
-
-#if __has_include("remote_fmt/remote_fmt.hpp")
-    #include "remote_fmt/remote_fmt.hpp"
-
-    #if __has_include(<aglio/type_descriptor.hpp>)
-        #include <aglio/type_descriptor.hpp>
-    #endif
-
-template<typename T>
-  concept PrintableRegister = requires {
-      { T::fmt_string } -> std::convertible_to<std::string_view>;
-      {
-          T::apply_fields([]<typename... Args>(Args&&...) { return true; })
-      } -> std::convertible_to<bool>;
-  }
-    #if __has_include(<aglio/type_descriptor.hpp>)
-  && aglio::Described<T>
-    #endif
-  ;
-
-template<PrintableRegister R>
-struct remote_fmt::formatter<R> {
-    template<typename FormatContext>
-    auto format(R const&,
-                FormatContext& ctx) {
-        return R::apply_fields_with_dim([&]<typename... Args>(Args&&... args) {
-            return format_to(ctx.out(), SC_LIFT(R::fmt_string), std::forward<Args>(args)...);
-        });
-    }
-};
-#endif
